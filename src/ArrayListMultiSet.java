95d4b4e52cebba0d39e2b7e131d03c35359a7fd7
godbil <47403236+godbil@users.noreply.github.com>
2023-09-18 16:53:32 -0400

    Up

---
+++ src/ArrayListMultiSet.java
@@ -0,0 +1,34 @@
+public class ArrayListMultiSet implements MultiSet{
+
+    private static
+
+    @Override
+    public boolean add(int item) {
+        return false;
+    }
+
+    @Override
+    public void remove(int item) {
+
+    }
+
+    @Override
+    public boolean contains(int item) {
+        return false;
+    }
+
+    @Override
+    public boolean is_empty() {
+        return false;
+    }
+
+    @Override
+    public int count(int item) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
